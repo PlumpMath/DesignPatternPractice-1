1ad985983bd46a1ec73345812c211664f7b520a4
PrivatorCheng <privatorcheng@hotmail.com>
2015-01-16 18:38:32 +0800

    ObserverMode check in 150116

--- DesignPatternLibrary/ObserverMode/MainBoard.cs
+++ DesignPatternLibrary/ObserverMode/MainBoard.cs
@@ -4,9 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DesignPatternLibrary
+namespace DesignPatternLibrary.ObserverMode
 {
-    public class Class1
+    public class MainBoard:IObserver , IDisplayData 
     {
+
     }
 }
